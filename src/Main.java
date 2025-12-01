047d65a50bec103ed46e2b1138e1af74bc45f920
Mileer leon <mileerdubanleon@gmail.com>
2025-12-01 17:17:10 -0500

    feat: add modify the access, set and get view main

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ import base.Proveedor;
 public class Main {
     public static void main(String[] Args){
         Proveedor proveedor = new Proveedor(1, "Daniel");
-
+        // get
         proveedor.getId();
         proveedor.getName();
+        // set
+        proveedor.setId(2);
+        proveedor.setName("Juan");
 
         System.out.println("El nombre del proveedor es: " + proveedor.getName());
         System.out.println("El id del proveedor es: " + proveedor.getId());
--- src/base/Proveedor.java
+++ src/base/Proveedor.java
@@ -24,4 +24,12 @@ public class Proveedor {
     public String getName(){
         return name;
     }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
 }
