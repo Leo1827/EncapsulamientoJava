600951cc4e742723efa112aa983a8dc5cd759ffd
Mileer leon <mileerdubanleon@gmail.com>
2025-12-03 13:22:38 -0500

    feat: add example alumno the access modify private and public

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,14 @@
+import base.Alumno;
 import base.Proveedor;
 import base.Persona;
 
 public class Main {
     public static void main(String[] Args){
-        Proveedor proveedor = new Proveedor(1, "Daniel", "Colombia");
-        // get
-        proveedor.getId();
-        proveedor.getName();
-        proveedor.name = "Mileer"; // no hay error es publico
-        proveedor.pais = "Colombia"; // Errores sobre protected
-        proveedor.id = 2; // Error sobre private
-        // set
-        proveedor.setId(2);
-        proveedor.setName("Juan");
+        Alumno alumno = new Alumno();
+        Alumno alumnoTwo = new Alumno(2, "Suscribete", "Rincon");
 
-        System.out.println("El nombre del proveedor es: " + proveedor.getName());
-        System.out.println("El id del proveedor es: " + proveedor.getId());
-
-        Persona persona = new Persona("Juan", 37,"Calle tornado","Colombia" );
-        persona.mostrarInfo();
+        System.out.println("Id: " + alumnoTwo.getId());
+        System.out.println("Nombre: " + alumnoTwo.getNombre());
+        System.out.println("Apellido: " + alumnoTwo.getApellido());
     }
 }
---
+++ src/base/Alumno.java
@@ -0,0 +1,42 @@
+package base;
+
+public class Alumno {
+    // Predeterminadamente deben ser private, para proteger los datos de otras clases
+    private int id;
+    private String nombre;
+    private String apellido;
+
+    public Alumno(){
+
+    }
+
+    public Alumno(int id, String nombre, String apellido){
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getApellido(){
+        return apellido;
+    }
+
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+}
