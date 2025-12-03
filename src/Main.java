4e4d9b9d79e19fefe85f893882a3667474b1a8b9
Mileer leon <mileerdubanleon@gmail.com>
2025-12-03 13:02:00 -0500

    Error: modify the access, main errors

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
 import base.Proveedor;
+import base.Persona;
 
 public class Main {
     public static void main(String[] Args){
-        Proveedor proveedor = new Proveedor(1, "Daniel");
+        Proveedor proveedor = new Proveedor(1, "Daniel", "Colombia");
         // get
         proveedor.getId();
         proveedor.getName();
+        proveedor.name = "Mileer"; // no hay error es publico
+        proveedor.pais = "Colombia"; // Errores sobre protected
+        proveedor.id = 2; // Error sobre private
         // set
         proveedor.setId(2);
         proveedor.setName("Juan");
 
         System.out.println("El nombre del proveedor es: " + proveedor.getName());
         System.out.println("El id del proveedor es: " + proveedor.getId());
+
+        Persona persona = new Persona("Juan", 37,"Calle tornado","Colombia" );
+        persona.mostrarInfo();
     }
 }
--- src/base/Proveedor.java
+++ src/base/Proveedor.java
@@ -1,14 +1,24 @@
 package base;
 
 public class Proveedor {
-
+    // Solo accesible dentro de la misma clase
+    // No accesible desde otras clases
+    // No accesibles desde clases hijas
     private int id;
+    // Accesible a otras clases
+    // Clases hijas
+    // Objetos externos
     public String name;
+    // Accesible a la clase proveedor
+    // Clases hijas
+    // No accesibles directamente desde otras clases externas
+    protected String pais;
 
     // Contructor
-    public Proveedor (int id,String name){
+    public Proveedor (int id,String name, String pais){
         this.id = id;
         this.name = name;
+        this.pais = pais;
     }
 
     public void mostrarProveedor(){
@@ -17,12 +27,18 @@ public class Proveedor {
     public void nombreProveedor(){
         System.out.printf("El nombre del proveedor es: %s", this.name);
     }
+    public void paisProveedor(){
+        System.out.printf("El pais del proveedor es: %s", this.pais);
+    }
 
     public int getId(){
         return id;
     }
     public String getName(){
         return name;
+    }
+    public String getPais(){
+        return pais;
     }
 
     public void setId(int id){
@@ -31,5 +47,9 @@ public class Proveedor {
 
     public void setName(String name){
         this.name = name;
+    }
+
+    public void setPais(String pais){
+        this.pais = pais;
     }
 }
